59343841e7aa2563c42dc01887b10bc3952ce06a
鲍方舟 <shakuganno00@163.com>
2018-08-27 21:59:45 +0800

    Last updated: 2018.08.27 22:00

---
+++ Sokoban/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sokoban
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdNewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void cmdRestartLevel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdSaveProgress_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdExit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdOpenLevelEditor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdModifyKeyBindings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdHowToPlay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
